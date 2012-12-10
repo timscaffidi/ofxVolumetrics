ae58561d4b9a5a79e8d4ac35da2b7c94424d2442
Timothy Scaffidi <timothy.scaffidi@gmail.com>
2012-12-09 21:05:51 -0500

    changed to stringification of shader code

--- src/ofxVolumetrics.cpp
+++ src/ofxVolumetrics.cpp
@@ -1,6 +1,8 @@
+
+
 #include "ofxVolumetrics.h"
 #include "ofMain.h"
-
+#define STRINGIFY( A) #A
 ofxVolumetrics::ofxVolumetrics()
 {
     quality = ofVec3f(1.0);
@@ -86,7 +88,119 @@ ofxVolumetrics::~ofxVolumetrics()
 
 void ofxVolumetrics::setup(int w, int h, int d, ofVec3f voxelSize)
 {
-    volumeShader.load("shaders/ofxVolumetrics");
+    string vertexShader = STRINGIFY(
+                                    varying vec3 cameraPosition;
+                                    void main()
+                                    {
+                                        gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+                                        gl_TexCoord[0] = gl_MultiTexCoord0; //poop
+                                        cameraPosition = (gl_ModelViewMatrixInverse * vec4(0.,0.,0.,1.)).xyz;
+                                    }); // END VERTEX SHADER STRINGIFY
+
+    string fragmentShader = STRINGIFY((#extension GL_ARB_texture_rectangle : enable \n
+                                        varying vec3 cameraPosition;
+
+                                        uniform sampler3D volume_tex;
+                                        uniform vec3 vol_d;
+                                        uniform vec2 bg_d;
+                                        uniform float zoffset;
+                                        uniform float quality;
+                                        uniform float threshold;
+                                        uniform float density;
+
+                                        struct Ray {
+                                            vec3 Origin;
+                                            vec3 Dir;
+                                        };
+
+                                        struct BoundingBox {
+                                            vec3 Min;
+                                            vec3 Max;
+                                        };
+
+                                        bool IntersectBox(Ray r, BoundingBox box, out float t0, out float t1)
+                                        {
+                                            vec3 invR = 1.0 / r.Dir;
+                                            vec3 tbot = invR * (box.Min-r.Origin);
+                                            vec3 ttop = invR * (box.Max-r.Origin);
+                                            vec3 tmin = min(ttop, tbot);
+                                            vec3 tmax = max(ttop, tbot);
+                                            vec2 t = max(tmin.xx, tmin.yz);
+                                            t0 = max(t.x, t.y);
+                                            t = min(tmax.xx, tmax.yz);
+                                            t1 = min(t.x, t.y);
+                                            return t0 <= t1;
+                                        }
+
+                                        void main()
+                                        {
+
+                                            vec3 minv = vec3(0.)+1./vol_d;
+                                            vec3 maxv = vec3(1.)-1./vol_d;
+                                            vec3 vec;
+                                            vec3 vold = (maxv-minv)*vol_d;
+                                            float vol_l = length(vold);
+
+                                            vec4 col_acc = vec4(0,0,0,0);
+                                            vec3 zOffsetVec = vec3(0.0,0.0,zoffset/vold.z);
+                                            vec3 backPos = gl_TexCoord[0].xyz;//*maxv+minv;
+                                            vec3 lookVec = normalize(backPos - cameraPosition);
+
+
+                                            Ray eye = Ray( cameraPosition, lookVec);
+                                            BoundingBox box = BoundingBox(minv, maxv);
+
+                                            float tnear, tfar;
+                                            IntersectBox(eye, box, tnear, tfar);
+                                            if (tnear < 0.15) tnear = 0.15;
+                                            if(tnear > tfar) discard;
+
+                                            vec3 rayStart = eye.Origin + eye.Dir * tnear;
+                                            vec3 rayStop = eye.Origin + eye.Dir * tfar;
+
+                                            vec3 dir = rayStop - rayStart; // starting position of the ray
+                                            vec = rayStart;
+                                            float dl = length(dir);
+                                            if(dl == clamp(dl,0.,vol_l)) {
+                                                float steps = floor(length(vold * dir) * quality);
+                                                vec3 delta_dir = dir/steps;
+                                                vec4 color_sample;
+                                                float aScale =  density/quality;
+
+                                                float random = fract(sin(gl_FragCoord.x * 12.9898 + gl_FragCoord.y * 78.233) * 43758.5453);
+                                                vec += delta_dir * random;
+
+                                                //raycast
+                                                for(int i = 0; i < int(steps); i++)
+                                                {
+                                                    color_sample = texture3D(volume_tex, vec + zOffsetVec);
+                                                    if(color_sample.a > threshold) {
+
+                                                        float oneMinusAlpha = 1. - col_acc.a;
+                                                        color_sample.a *= aScale;
+                                                        col_acc.rgb = mix(col_acc.rgb, color_sample.rgb * color_sample.a, oneMinusAlpha);
+                                                        col_acc.a += color_sample.a * oneMinusAlpha;
+                                                        col_acc.rgb /= col_acc.a;
+                                                        if(col_acc.a >= 1.0) {
+                                                            break; // terminate if opacity > 1
+                                                        }
+                                                    }
+                                                    vec += delta_dir;
+                                                }
+                                            }
+                                            // export the rendered color
+                                            gl_FragColor = col_acc;//vec4(abs(rayStop-rayStart),1.);
+                                        } )); // END FRAGMENT SHADER STRINGIFY
+
+    // For whatever reason, the stringify macro takes the fragment shader code as 2 arguments,
+    // wrapping it in () makes it compile, so trim them off
+    fragmentShader = fragmentShader.substr(1,fragmentShader.size()-2);
+
+    volumeShader.unload();
+    volumeShader.setupShaderFromSource(GL_VERTEX_SHADER, vertexShader);
+    volumeShader.setupShaderFromSource(GL_FRAGMENT_SHADER, fragmentShader);
+    volumeShader.linkProgram();
+
     volWidth = renderWidth = w;
     volHeight = renderHeight = h;
     volDepth = d;
