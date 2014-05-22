326b22fb88783099caa28d572819679ac32cd390
Timothy Scaffidi <timothy.scaffidi@gmail.com>
2014-05-22 13:45:16 -0400

    always set pixel store in load data method

--- src/ofxTexture3d.cpp
+++ src/ofxTexture3d.cpp
@@ -91,7 +91,7 @@ void ofxTexture3d::loadData(void * data, int w, int h, int d, int xOffset, int y
         return;
     }
 
-
+    ofSetPixelStorei(w, 1, ofGetNumChannelsFromGLFormat(glFormat));
     glEnable(texData.textureTarget);
     glBindTexture(texData.textureTarget, (GLuint) texData.textureID);
     glTexSubImage3D(texData.textureTarget, 0, xOffset, yOffset, zOffset, w, h, d, texData.glType, texData.pixelType, data);
